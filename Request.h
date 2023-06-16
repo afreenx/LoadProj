f6935eba4a85d67388e74d090df1e65307544c60
a3pmfatima <a3pmfatima@tamu.edu>
2023-06-16 18:16:00 -0500

    IP numbers in request randomized

--- Request.h
+++ Request.h
@@ -1,12 +1,18 @@
 #ifndef REQUEST_H
 #define REQUEST_H
 
+#include <cstdlib>
 #include <string>
 
+
 struct Request {
-    std::string inputIP;
-    std::string outputIP;
-    int processingTime;
+    std::string inputIP = std::to_string(rand() % 256) + "." + std::to_string(rand() % 256) + "." +
+        std::to_string(rand() % 256) + "." + std::to_string(rand() % 256); 
+
+    std::string outputIP = std::to_string(rand() % 256) + "." + std::to_string(rand() % 256) + "." +
+        std::to_string(rand() % 256) + "." + std::to_string(rand() % 256); 
+
+    int processingTime; 
 };
 
 #endif
