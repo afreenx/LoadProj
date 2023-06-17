63317ff3aec61f5d49680a9832a004fe920244be
afreenx <a3pmfatima@gmail.com>
2023-06-16 23:50:00 -0500

    request struct doxy

--- Request.h
+++ Request.h
@@ -5,14 +5,21 @@
 #include <string>
 
 
+/**
+ * @brief Represents a request with input and output IP addresses and processing time.
+ *
+ * The Request struct represents a request with input and output IP addresses and a processing time. It generates random
+ * IP addresses within the range of 0.0.0.0 to 255.255.255.255 for both the input and output IP addresses. The processing
+ * time is an integer value.
+ */
 struct Request {
     std::string inputIP = std::to_string(rand() % 256) + "." + std::to_string(rand() % 256) + "." +
-        std::to_string(rand() % 256) + "." + std::to_string(rand() % 256); 
+        std::to_string(rand() % 256) + "." + std::to_string(rand() % 256);
 
     std::string outputIP = std::to_string(rand() % 256) + "." + std::to_string(rand() % 256) + "." +
-        std::to_string(rand() % 256) + "." + std::to_string(rand() % 256); 
+        std::to_string(rand() % 256) + "." + std::to_string(rand() % 256);
 
-    int processingTime; 
+    int processingTime; /**< The processing time for the request. */
 };
 
 #endif
