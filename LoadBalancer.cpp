ee25b3f0a944612e2c97ad8096795ba2fd6c0138
afreenx <a3pmfatima@gmail.com>
2023-06-17 00:56:56 -0500

    Load balancer doxy

--- LoadBalancer.cpp
+++ LoadBalancer.cpp
@@ -1,26 +1,54 @@
 #include "LoadBalancer.h"
 //loadbalancer class that manages the webservers and the request queue
 
+/**
+ * @brief Constructs a new LoadBalancer object.
+ *
+ * The LoadBalancer class manages the web servers and the request queue.
+ */
 LoadBalancer::LoadBalancer() {
-    //my constructor with the time
-    this->time = 0;
+    this->time = 0; /**< The current time of the load balancer. */
 }
+
+/**
+ * @brief Retrieves the current time of the load balancer.
+ *
+ * @return The current time of the load balancer.
+ */
 int LoadBalancer::getTime() {
     return this->time;
 }
 
+/**
+ * @brief Updates the time of the load balancer by incrementing it by 1.
+ */
 void LoadBalancer::updateTime() {
     this->time++;
 }
 
+/**
+ * @brief Checks if the request queue is empty.
+ *
+ * @return True if the request queue is empty, false otherwise.
+ */
 bool LoadBalancer::emptyQueueCheck() {
     return this->reqQueue.empty();
 }
 
+/**
+ * @brief Retrieves the size of the request queue.
+ *
+ * @return The size of the request queue.
+ */
 int LoadBalancer::sizeQueue() {
     return this->reqQueue.size();
 }
 
+/**
+ * @brief Pops the next request from the request queue.
+ *
+ * @return A pointer to the next request in the queue, or nullptr if the queue is empty.
+ */
 Request* LoadBalancer::popRequest() {
     if (this->emptyQueueCheck()) {
         return nullptr;
@@ -31,7 +59,11 @@ Request* LoadBalancer::popRequest() {
     return nextReq;
 }
 
+/**
+ * @brief Pushes a request into the request queue.
+ *
+ * @param req A pointer to the request to be pushed into the queue.
+ */
 void LoadBalancer::pushRequest(Request* req) {
     this->reqQueue.push(req);
 }
-
--- LoadBalancer.h
+++ LoadBalancer.h
@@ -5,19 +5,59 @@
 #include "Webserver.h"
 #include <queue>
 
+/**
+ * @brief The LoadBalancer class manages the web servers and the request queue.
+ */
 class LoadBalancer {
 public:
+    /**
+     * @brief Constructs a new LoadBalancer object.
+     */
     LoadBalancer();
+
+    /**
+     * @brief Retrieves the current time of the load balancer.
+     *
+     * @return The current time of the load balancer.
+     */
     int getTime();
+
+    /**
+     * @brief Updates the time of the load balancer by incrementing it by 1.
+     */
     void updateTime();
+
+    /**
+     * @brief Retrieves the size of the request queue.
+     *
+     * @return The size of the request queue.
+     */
     int sizeQueue();
+
+    /**
+     * @brief Checks if the request queue is empty.
+     *
+     * @return True if the request queue is empty, false otherwise.
+     */
     bool emptyQueueCheck();
+
+    /**
+     * @brief Pops the next request from the request queue.
+     *
+     * @return A pointer to the next request in the queue, or nullptr if the queue is empty.
+     */
     Request* popRequest();
+
+    /**
+     * @brief Pushes a request into the request queue.
+     *
+     * @param req A pointer to the request to be pushed into the queue.
+     */
     void pushRequest(Request* req);
 
 private:
-    int time;
-    std::queue<Request*> reqQueue;
+    int time; /**< The current time of the load balancer. */
+    std::queue<Request*> reqQueue; /**< The queue of requests to be processed by the load balancer. */
 };
 
 #endif
